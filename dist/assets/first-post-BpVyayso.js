6c9590b69b6e966d4390b6cbf2e41c6bccc647ec
thjoncpt025 <thjoncpt025@student.wethinkcode.co.za>
2026-03-25 14:24:57 +0200

    updated

---
+++ dist/assets/first-post-BpVyayso.js
@@ -0,0 +1,13 @@
+const n=`# Welcome to My Blog
+
+This is my first developer update!
+
+## What I’m working on
+- Improving my portfolio
+- Building a chatbot
+- Connecting GitHub API
+
+
+\`\`\`js
+console.log("Hello world");
+`;export{n as default};
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  base: "/final_portfolio/", // important for GitHub Pages
   server: {
     port: 5173,
     host: true
